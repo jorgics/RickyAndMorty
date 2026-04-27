a4c0f31e6f9b58ecf35515b8a0e8003154753b4d
jnueno <jnueno14@gmail.com>
2026-04-27 21:22:34 +0200

    JNA - Add BaseViewModel.kt

---
+++ app/src/main/java/com/practice/rickyandmorty/core/ui/viewmodel/BaseViewModel.kt
@@ -0,0 +1,20 @@
+package com.practice.rickyandmorty.core.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+abstract class BaseViewModel<Intent, State>(
+    initialState: State
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(initialState)
+    val state: StateFlow<State> = _state
+
+    protected fun setState(reducer: State.() -> State) {
+        _state.update(reducer)
+    }
+
+    abstract fun sendIntent(intent: Intent)
+}

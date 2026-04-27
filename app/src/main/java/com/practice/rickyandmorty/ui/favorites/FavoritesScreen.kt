a5893b247f8e3ab46fffed75f66c0a5cb2be413a
jnueno <jnueno14@gmail.com>
2026-04-27 21:23:10 +0200

    JNA - Add Screens FavoritesScreen.kt, ProfileScreen.kt, SearchScreen.kt.

---
+++ app/src/main/java/com/practice/rickyandmorty/ui/favorites/FavoritesScreen.kt
@@ -0,0 +1,13 @@
+package com.practice.rickyandmorty.ui.favorites
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FavoritesScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+
+    }
+}

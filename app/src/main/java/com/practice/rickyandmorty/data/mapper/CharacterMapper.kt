236c470bd65a2a5e890722233944d86a8abbbdb0
jnueno <jnueno14@gmail.com>
2026-05-21 10:45:23 +0200

    JNA - Clean code.

--- app/src/main/java/com/practice/rickyandmorty/data/mapper/CharacterMapper.kt
+++ app/src/main/java/com/practice/rickyandmorty/data/mapper/CharacterMapper.kt
@@ -1,37 +1,8 @@
 package com.practice.rickyandmorty.data.mapper
 
 import com.practice.rickyandmorty.data.remote.model.CharacterDto
-import com.practice.rickyandmorty.data.remote.model.LocationDto
-import com.practice.rickyandmorty.data.remote.model.OriginDto
 import com.practice.rickyandmorty.database.entities.CharacterEntity
 import com.practice.rickyandmorty.domain.model.Character
-import com.practice.rickyandmorty.domain.model.Location
-import com.practice.rickyandmorty.domain.model.Origin
-
-fun CharacterDto.toDomain(): Character = Character(
-    id = id,
-    name = name,
-    status = status,
-    species = species,
-    type = type,
-    gender = gender,
-    image = image,
-    origin = origin?.toDomain(),
-    location = location?.toDomain(),
-    episode = episode,
-    url = url,
-    created = created
-)
-
-fun LocationDto.toDomain(): Location = Location(
-    name = name,
-    url = url
-)
-
-fun OriginDto.toDomain(): Origin = Origin(
-    name = name,
-    url = url
-)
 
 fun CharacterDto.toEntity(queryKey: String): CharacterEntity = CharacterEntity(
     id = id,
--- app/src/main/java/com/practice/rickyandmorty/data/remote/RickAndMortyService.kt
+++ app/src/main/java/com/practice/rickyandmorty/data/remote/RickAndMortyService.kt
@@ -3,14 +3,10 @@ package com.practice.rickyandmorty.data.remote
 import com.practice.rickyandmorty.data.remote.model.CharacterDto
 import com.practice.rickyandmorty.data.remote.response.RickyAndMortyResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 interface RickAndMortyService {
-    @GET("character/{id}")
-    suspend fun getCharacterById(@Path("id") id: Int): CharacterDto
-
     @GET("character")
     suspend fun getCharactersByFilter(
         @Query("page") page: Int,
